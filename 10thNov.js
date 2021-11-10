7646e4f744592439bd8f3547629b5c76adae87af
sanjubpujeri <sanjubpujeri153@gmail.com>
2021-11-10 22:01:20 +0530

    okay

---
+++ 10thNov.js
@@ -0,0 +1,7 @@
+let contact = { HouseNo : 305, Village: "Piranawadi"};
+console.log(contact.HouseNo);
+console.log(contact.Village);
+console.log(contact.State);
+
+//If we are trying to access the object property which is not exist in the object we will get error as undefined//
+//If you are trying access a object method which is not exists in object we will get - Error//
