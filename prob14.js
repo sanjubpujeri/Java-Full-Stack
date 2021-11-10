fa7e03b5a8fabd2b29ff352f2e7ee6abbe69af52
sanjubpujeri <sanjubpujeri153@gmail.com>
2021-11-10 20:43:04 +0530

    Today's practice

--- prob14.js
+++ prob14.js
@@ -1,15 +1,10 @@
-const number = ('Enater a positive number : ');
+//Program to print fiboancci serie up to a given number//
+const number=17;
 let n1=0, n2=1, nextTerm;
-console.log('Fibonacci series:');
-console.log(n1);
-console.log(n2);
-
-nextTerm = n1+n2;
-
-while(nextTerm <= number){
-    console.log(nextTerm);
+console.log('fibonacci series :');
+for(i=1; i<=number;  i++){
+    console.log(n1);
+    nextTerm= n1+n2;
     n1=n2;
-    n2= nextTerm;
-    nextTerm = n1+n2;
-
+    n2=nextTerm;
 }
